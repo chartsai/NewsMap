477e1d21d7f2a6914b0cc5750c222441b31a5001
cha122977 <cha122977@gmail.com>
2015-08-22 18:24:09 +0800

    Add query task used to parsing

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import logging
+
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from google.appengine.api import taskqueue
 from news import News
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 @app.route('/testNews')
 def testNews():
-    news = News()
     result = news.loadfromdb("URL")
     return_str = str(news.title) + str(news.content)
     return "Result: " + result + "<br>" + return_str
@@ -19,6 +23,17 @@ def addTestNews():
     result = news.writetodb()
     return "Write News(URL) to db, result is: " + result
 
+@app.route('/trigger_background_parsing')
+def parserWorker():
+    taskqueue.add(queue_name='default', url='/perform_parsing', params={})
+    return "Recieve: "
+
+@app.route('/perform_parsing', methods=['POST'])
+def perform_parsing():
+    logging.info("perform_parsing start")
+    # TODO put looping parsing code here.
+    logging.info("perform_parsing end")
+    return "Performing background parsing end"
 
 @app.errorhandler(404)
 def page_not_found(e):
