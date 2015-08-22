45595c7f6202918529846241bc73e068033515b5
cha122977 <cha122977@gmail.com>
2015-08-22 13:12:19 +0800

    Provide API to read/write News into DB
    Also provide 2 test url:
    http:/[address]/addTestNews
    http:/[address]/testNews
    to test if DB is work.

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from news import News
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+@app.route('/testNews')
+def testNews():
+    news = News()
+    news.loadfromdb("URL")
+    return_str = str(news.title) + str(news.content)
+    return return_str
+
+@app.route('/addTestNews')
+def addTestNews():
+    news = News("URL")
+    news.writetodb()
+    return "Write News(URL) to db"
+
 
 @app.errorhandler(404)
 def page_not_found(e):
--- news.py
+++ news.py
@@ -5,11 +5,61 @@ sys.setdefaultencoding('utf8')
 from google.appengine.ext import db
 
 class NewsModel(db.Model):
-    title = db.StringProperty(required=True)
-    content = db.StringProperty()
-    populatiry = db.IntegerProperty()
-    news_datetime = db.DateTimeProperty()
     news_url = db.StringProperty(required=True)
+    title = db.StringProperty()
+    content = db.StringProperty()
+    popularity = db.IntegerProperty()
+    news_datetime = db.DateTimeProperty()
     news_first_image_url = db.StringProperty()
     news_source = db.StringProperty()
-    download_datetime = db.DateTimeProperty(auto_now_add=True)
+    parse_datetime = db.DateTimeProperty(auto_now_add=True)
+
+class News:
+    def __init__(self, news_url="", title="News", content="", popularity=0,
+                 news_datetime="", news_first_image_url="", news_source="Unknown"):
+        self.setnewsdata(news_url, title, content, popularity, news_datetime,
+                         news_first_image_url, news_source)
+
+
+    def setnewsdata(self, news_url="", title="News", content="", popularity=0,
+                    news_datetime="", news_first_image_url="", news_source="Unknown"):
+        self.news_url = news_url
+        self.title = title
+        self.content = content
+        self.popularity = popularity
+        self.news_datetime = news_datetime
+        self.news_first_image_url = news_first_image_url
+        self.news_source = news_source
+
+    def writetodb(self):
+        news_entry = NewsModel.all().filter("news_url =", self.news_url).get()
+        if news_entry == None:
+            # create new entry
+            key = NewsModel(news_url = self.news_url,
+                            title = self.title,
+                            content = self.content,
+                            popularity = self.popularity,
+                            news_datetime = self.news_datetime,
+                            news_first_image_url = self.news_first_image_url,
+                            news_source = self.news_source)
+            db.put(key)
+        else:
+            # update exist entry
+            news_entry.title = self.title
+            news_entry.content = self.content
+            news_entry.popularity = self.popularity
+            news_datetime = self.news_datetime
+            news_entry.news_first_image_url = self.news_first_image_url
+            news_entry.news_source = self.news_source
+            db.put(news_entry)
+
+    def loadfromdb(self, url):
+        news_entry = NewsModel.all().filter("news_url =", self.news_url).get()
+        if news_entry != None:
+            self.news_url = news_entry.news_url
+            self.title = news_entry.title
+            self.content = news_entry.content
+            self.popularity = news_entry.popularity
+            self.news_datetime = news_entry.news_datetime
+            self.news_first_image_url = news_entry.news_first_image_url
+            self.news_source = news_entry.news_source
